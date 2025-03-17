956b25f7f5b8c99f9c73f91ddccab66573ff0448
Riley Connors <rilesconnors@gmail.com>
2025-03-17 10:52:50 -0500

    refactor to WagoDriver

--- driver/src/driver.rs
+++ driver/src/driver.rs
@@ -1,5 +1,17 @@
+use super::properties::TargetProperties;
+
 #[derive(Debug)]
-pub struct PlcDriver {
-    pub properties: Properties,
+pub struct WagoDriver {
+    pub properties: TargetProperties,
     pub queue_handle: Option<tokio::task::JoinHandle<()>>,
+}
+
+impl WagoDriver {
+    pub fn new(properties: TargetProperties) -> Self {
+        Self {
+            properties,
+            queue_handle: None,
+        }
+    }
+    pub fn connect(&mut self) {}
 }
---
+++ driver/src/properties.rs
@@ -0,0 +1,26 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct TargetProperties {
+    pub ip: String,
+}
+
+impl TargetProperties {
+    pub fn new(ip: &str) -> Self {
+        return Self {
+            ip: ip.to_string(),
+        };
+    }
+}
+
+impl Default for TargetProperties {
+    fn default() -> Self {
+        return Self {
+            ip: "10.22.1.201:502".to_string(),
+        };
+    }
+}
+
+/*
+
+*/
